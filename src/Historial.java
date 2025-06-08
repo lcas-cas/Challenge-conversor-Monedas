474f2a6c7f46b2c791b8ddab03c17c0045b20128
lcas-cas <lucas_cardenas05@outlook.com>
2025-06-08 03:37:55 -0600

    Solucion problemas con Historial y validaciones en Principal

---
+++ src/Historial.java
@@ -0,0 +1,54 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Historial {
+    private final File archivo = new File("historial.txt");
+
+    public void crearArchivoSiNoExiste(){
+        try {
+            if (archivo.createNewFile()){
+                System.out.println("Archivo historial.txt creado.");
+            }
+
+        }catch (IOException  e){
+            System.out.println("Error al crear el archivo: "+ e.getMessage());
+        }
+    }
+
+
+    public void guardarConversion(String base,String destino, double monto,
+                                 double resultado,String fecha ){
+
+        try(FileWriter escritura = new FileWriter(archivo,true)) {
+            escritura.write(String.format("%s - %.2f %s => %.2f %s\n",
+                    fecha,monto,base,resultado,destino));
+
+        } catch (IOException e) {
+            System.out.println("Error al guardar la conversasión "+ e.getMessage());
+        }
+    }
+
+    public void mostrarHistorial(){
+        if (!archivo.exists()){
+            System.out.println("No hay historial de conversiones todavía.");
+            return;
+        }
+
+        System.out.println("     ===== Historial de conversiones =====      ║");
+
+
+        try(Scanner lector = new Scanner(archivo)) {
+            while(lector.hasNextLine()){
+                System.out.println(lector.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el historial: "+e.getMessage());
+        }
+
+    }
+
+
+}
--- src/Principal.java
+++ src/Principal.java
@@ -4,6 +4,9 @@ public class Principal {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         ConsultaMonedas convertir = new ConsultaMonedas();
+        Historial historial = new Historial();
+        historial.crearArchivoSiNoExiste();
+        Fecha fecha = new Fecha();
 
         String monedaBase = "";
         String monedaDestino = "";
@@ -24,9 +27,10 @@ public class Principal {
                 ║ 10. Boliviano boliviano => Dólar                       ║
                 ║ 11. Peso mexicano => Colón costarricense               ║
                 ║ 12. Colón costarricense => Peso mexicano               ║
-                ║ 13. Salir                                              ║
+                ║ 13. Ver historial de conversiones                      ║
+                ║ 14. Salir                                              ║
                 ╚════════════════════════════════════════════════════════╝
-                Seleccione una opción (1-13):
+                Seleccione una opción (1-14):
                 """;
 
 
@@ -41,103 +45,116 @@ public class Principal {
             }
 
             opcion = teclado.nextInt();
-            if (opcion >= 1 && opcion <= 12) {
-                System.out.println("Ingrese el valor que deseas convertir: ");
-                double monto = teclado.nextDouble();
 
-                if (monto<=0){
-                    System.out.println("El monto debe ser mayor a 0. Intente de nuevo\n");
-                    continue;
-                }
-
-
-                switch (opcion) {
-                    case 1: {
-                        monedaBase = "USD";
-                        monedaDestino = "ARS";
-                        break;
-                    }
-                    case 2: {
-                        monedaBase = "ARS";
-                        monedaDestino = "USD";
-                        break;
-                    }
-                    case 3: {
-                        monedaBase = "USD";
-                        monedaDestino = "BRL";
-                        break;
-                    }
-                    case 4: {
-                        monedaBase = "BRL";
-                        monedaDestino = "USD";
-                        break;
-                    }
-                    case 5: {
-                        monedaBase = "USD";
-                        monedaDestino = "COP";
-                        break;
-                    }
-                    case 6: {
-                        monedaBase = "COP";
-                        monedaDestino = "USD";
-                        break;
-                    }
-                    case 7: {
-                        monedaBase = "USD";
-                        monedaDestino = "CRC";
-                        break;
-                    }
-                    case 8: {
-                        monedaBase = "CRC";
-                        monedaDestino = "USD";
-                        break;
-                    }
-                    case 9: {
-                        monedaBase = "USD";
-                        monedaDestino = "BOB";
-                        break;
-                    }
-                    case 10: {
-                        monedaBase = "BOB";
-                        monedaDestino = "USD";
-                        break;
-                    }
-                    case 11:{
-                        monedaBase="MXN";
-                        monedaDestino="CRC";
-                        break;
-                    }
-                    case 12:{
-                        monedaBase="CRC";
-                        monedaDestino="MXN";
-                        break;
-                    }
-
-                }
-
-                MonedasApi resultado = convertir.
-                        convertirMonedasConMonto(monedaBase, monedaDestino, monto);
-
-                System.out.printf("El valor %.2f %s corresponde a %.2f %s\n",
-                        monto,
-                        resultado.base_code(),
-                        resultado.conversion_result(),
-                        resultado.target_code());
-
-
-
-
-            }else if (opcion==13){
-                System.out.println("Gracias por usar el conversor");
-            }else{
-                System.out.println("Opción inválida. Intente de nuevo.\n");
+            if (opcion==13){
+                historial.mostrarHistorial();
+                continue;// volver al menu
             }
 
+            //Opcion 14 salir
+            if (opcion==14){
+                System.out.println("Gracias por usar el conversor");
+                break;
+            }
+
+            //Validar el rango
+            if (opcion < 1 || opcion > 14) {
+                System.out.println("Opción invalida. Intente de nuevo\n");
+                continue;
+            }
+
+            //Para las opciones de 1 a 12: pedir monto y convertir
+            System.out.println("Ingrese el valor que deseas convertir: ");
+            double monto = teclado.nextDouble();
+
+            if (monto<=0){
+                System.out.println("El monto debe ser mayor a 0. Intente de nuevo\n");
+                continue;
+            }
+
+            //Define monedas según opción.
+            switch (opcion) {
+                case 1: {
+                    monedaBase = "USD";
+                    monedaDestino = "ARS";
+                    break;
+                }
+                case 2: {
+                    monedaBase = "ARS";
+                    monedaDestino = "USD";
+                    break;
+                }
+                case 3: {
+                    monedaBase = "USD";
+                    monedaDestino = "BRL";
+                    break;
+                }
+                case 4: {
+                    monedaBase = "BRL";
+                    monedaDestino = "USD";
+                    break;
+                }
+                case 5: {
+                    monedaBase = "USD";
+                    monedaDestino = "COP";
+                    break;
+                }
+                case 6: {
+                    monedaBase = "COP";
+                    monedaDestino = "USD";
+                    break;
+                }
+                case 7: {
+                    monedaBase = "USD";
+                    monedaDestino = "CRC";
+                    break;
+                }
+                case 8: {
+                    monedaBase = "CRC";
+                    monedaDestino = "USD";
+                    break;
+                }
+                case 9: {
+                    monedaBase = "USD";
+                    monedaDestino = "BOB";
+                    break;
+                }
+                case 10: {
+                    monedaBase = "BOB";
+                    monedaDestino = "USD";
+                    break;
+                }
+                case 11:{
+                    monedaBase="MXN";
+                    monedaDestino="CRC";
+                    break;
+                }
+                case 12:{
+                    monedaBase="CRC";
+                    monedaDestino="MXN";
+                    break;
+                }
+
+            }
+
+            //Realiza conversión y garda en historial
+            MonedasApi resultado = convertir.
+                    convertirMonedasConMonto(monedaBase, monedaDestino, monto);
+
+            System.out.printf("El valor %.2f %s corresponde a %.2f %s\n",
+                    monto,
+                    resultado.base_code(),
+                    resultado.conversion_result(),
+                    resultado.target_code());
+
+            historial.guardarConversion(resultado.base_code(),
+                    resultado.target_code(),
+                    monto,
+                    resultado.conversion_result(),
+                    fecha.obtenerFecha());
 
 
-        }while(opcion !=13);
-
-
+        }while(true);
 
 
 
